c130131914025f6bd79286d8cf4e54812e439f04
uo283681 <uo283681@uniovi.es>
2022-09-28 16:25:39 +0200

    carga con

--- src/demo1/Cdemo.java
+++ src/demo1/Cdemo.java
@@ -3,6 +3,6 @@ package demo1;
 public class Cdemo {
 	public static void main(String[] args) {
 		System.out.println("linea 1");
-		System.out.println("linea 2");
+		System.out.println("linea master");
 	}
 }
