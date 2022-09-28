3a7672be428e918ad60d773b966b552f3c5db091
uo283681 <uo283681@uniovi.es>
2022-09-28 16:30:25 +0200

    solucionado

--- src/demo1/Cdemo.java
+++ src/demo1/Cdemo.java
@@ -3,10 +3,7 @@ package demo1;
 public class Cdemo {
 	public static void main(String[] args) {
 		System.out.println("linea 1");
-<<<<<<< HEAD
 		System.out.println("linea master");
-=======
-		System.out.println("linea master");
->>>>>>> refs/heads/cambio3
+
 	}
 }
