c6361ba8c18a1c95df787965178ce51a70f705ab
uo283681 <uo283681@uniovi.es>
2022-09-28 16:35:11 +0200

    nueva rama

--- src/demo1/Cdemo.java
+++ src/demo1/Cdemo.java
@@ -4,6 +4,7 @@ public class Cdemo {
 	public static void main(String[] args) {
 		System.out.println("linea 1");
 		System.out.println("linea master");
+		System.out.println("nueva linea master");
 
 	}
 }
